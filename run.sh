8c85f70c6535b02f530ba63ee16dc9ba56a8b0f1
Lucas Mellos Carlos <lucasmelloscarlos@gmail.com>
2023-01-26 14:21:45 -0300

    fix relative paths

--- run.sh
+++ run.sh
@@ -16,6 +16,6 @@ cp $INPUT_PROJECT_NAME-$VERSION/$INPUT_PROJECT_NAME.spec /data/rpmbuild/SPECS/
 sed -i "s/Release:        1%{?dist}/Release:        $GITHUB_RUN_ID.$GITHUB_SHA/g" /data/rpmbuild/SPECS/$INPUT_PROJECT_NAME.spec
 cat /data/rpmbuild/SPECS/$INPUT_PROJECT_NAME.spec
 rpmbuild -ba /data/rpmbuild/SPECS/$INPUT_PROJECT_NAME.spec
-mv /data/rpmbuild/RPMS/x86_64/*.rpm /data
-mv /data/rpmbuild/SRPMS/*.rpm /data
+mv /data/rpmbuild/RPMS/x86_64/*.rpm $GITHUB_WORKSPACE
+mv /data/rpmbuild/SRPMS/*.rpm $GITHUB_WORKSPACE
 ls -la
