06ffc28269e7b45eca3d0713d023c47317747b79
Lucas Mellos Carlos <lucasmelloscarlos@gmail.com>
2023-01-26 14:13:34 -0300

    fix relative paths

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ echo $VERSION
 mkdir -p $INPUT_PROJECT_NAME-$VERSION
 cp -R $GITHUB_WORKSPACE/* $GITHUB_WORKSPACE/$INPUT_PROJECT_NAME-$VERSION
 tar -zcvf $INPUT_PROJECT_NAME-$VERSION.tar.gz $INPUT_PROJECT_NAME-$VERSION
-mv $INPUT_PROJECT_NAME-$VERSION.tar.gz rpmbuild/SOURCES/
-cp $INPUT_PROJECT_NAME-$VERSION/$INPUT_PROJECT_NAME.spec rpmbuild/SPECS/
-cd rpmbuild
-sed -i "s/Release:        1%{?dist}/Release:        $GITHUB_RUN_ID.$GITHUB_SHA/g" SPECS/$INPUT_PROJECT_NAME.spec
-cat SPECS/$INPUT_PROJECT_NAME.spec
-rpmbuild -ba SPECS/$INPUT_PROJECT_NAME.spec
-mv RPMS/x86_64/*.rpm /data
-mv SRPMS/*.rpm /data
+mv $INPUT_PROJECT_NAME-$VERSION.tar.gz /data/rpmbuild/SOURCES/
+cp $INPUT_PROJECT_NAME-$VERSION/$INPUT_PROJECT_NAME.spec /data/rpmbuild/SPECS/
+
+sed -i "s/Release:        1%{?dist}/Release:        $GITHUB_RUN_ID.$GITHUB_SHA/g" /data/rpmbuild/SPECS/$INPUT_PROJECT_NAME.spec
+cat /data/rpmbuild/SPECS/$INPUT_PROJECT_NAME.spec
+rpmbuild -ba /data/rpmbuild/SPECS/$INPUT_PROJECT_NAME.spec
+mv /data/rpmbuild/RPMS/x86_64/*.rpm /data
+mv /data/rpmbuild/SRPMS/*.rpm /data
 ls -la
